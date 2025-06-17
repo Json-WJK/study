44f0e1cb0a2c2998f52fe709483ba9acfa305a38
王俊坤 <congti422@163.com>
2025-06-17 14:53:44 +0800

    完成秒传前端页面

--- vite-project/src/router/index.ts
+++ vite-project/src/router/index.ts
@@ -2,7 +2,8 @@ import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
 
 // 定义路由类型（可选）
 const routes: RouteRecordRaw[] = [
-  { path: '/todoList', name: 'Home', component: () => import('@/views/todoList/main.vue') }, // todoList
+  { path: '/todoList', name: 'todoList', component: () => import('@/views/todoList/main.vue') }, // todoList
+  { path: '/fileResume', name: 'fileResume', component: () => import('@/views/fileResume/main.vue') }, // fileResume
 ]
 
 const router = createRouter({
---
+++ vite-project/src/views/fileResume/main.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>秒传</h1>
+    <p>文件秒传功能可以通过比较文件的哈希值来实现。以下是一个简单的实现思路：</p>
+    <!-- 文件上传 -->
+    <input type="file" @change="handleFileChange" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { calculateFileHash } from "@/utils";
+const file = ref<File | null>(null);
+const handleFileChange = (event: Event) => {
+  const target = event.target as HTMLInputElement;
+  if (target.files && target.files.length > 0) {
+    file.value = target.files[0];
+    console.log("选择的文件:", file.value);
+    calculateFileHash(file.value).then((hash) => {
+      console.log("文件哈希值:", hash);
+    });
+  }
+};
+</script>
+
+<style>
+</style>
